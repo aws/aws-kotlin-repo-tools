3f388e27715c9637c6303318014230d1796529c8
0marperez <operez899999@gmail.com>
2025-09-18 16:30:11 -0400

    feedback

--- ktlint/minor-version-rules/src/main/kotlin/software/aws/ktlint/rules/DeprecatedApiRule.kt
+++ ktlint/minor-version-rules/src/main/kotlin/software/aws/ktlint/rules/DeprecatedApiRule.kt
@@ -9,6 +9,8 @@ import com.pinterest.ktlint.rule.engine.core.api.ElementType
 import com.pinterest.ktlint.rule.engine.core.api.Rule
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import java.io.File
+import java.util.Properties
 
 /**
  * Matches @DeprecatedUntilVersion with either named args (major=x, minor=y) or positional args (x, y)
@@ -28,7 +30,11 @@ class DeprecatedApiRule : Rule(RuleId("minor-version-strategy-rules:deprecated-a
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
         if (node.elementType == ElementType.ANNOTATION_ENTRY) {
-            val sdkVersion = System.getProperty("sdkVersion").split(".")
+            val gradleProperties = Properties().apply {
+                load(File("gradle.properties").inputStream())
+            }
+
+            val sdkVersion = gradleProperties.getProperty("sdkVersion").split(".")
             val majorVersion = sdkVersion[0].toInt()
             val minorVersion = sdkVersion[1].toInt()
 
