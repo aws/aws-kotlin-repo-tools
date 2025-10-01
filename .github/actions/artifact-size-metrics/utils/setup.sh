9cb1f3a785c41b18395352699b8830b4a0a848df
0marperez <operez899999@gmail.com>
2025-10-01 19:06:12 -0400

    enable command tracing

--- .github/actions/artifact-size-metrics/utils/setup.sh
+++ .github/actions/artifact-size-metrics/utils/setup.sh
@@ -1,5 +1,6 @@
+# Exit if non zero exit code or if env var is missing, and enable command tracing
 setup() {
-  # Exit if non zero exit code or if env var is missing
   set -u
   set -e
+  set -x
 }
