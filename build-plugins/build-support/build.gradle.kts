3575291bc14651a98e4917f80c13122c5e798c6a
Matas Lauzadis <lauzmata@amazon.com>
2025-10-02 17:45:08 -0400

    Add kotlin("plugin.serialization") to plugins

--- build-plugins/build-support/build.gradle.kts
+++ build-plugins/build-support/build.gradle.kts
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `kotlin-dsl`
     kotlin("jvm")
+    kotlin("plugin.serialization") version embeddedKotlinVersion
     `java-gradle-plugin`
 }
 
