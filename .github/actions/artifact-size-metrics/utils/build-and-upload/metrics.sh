808ea091a072d3ed55ce424acab200fd46c4a956
0marperez <operez899999@gmail.com>
2025-10-02 13:11:16 -0400

    polish metrics collection

--- .github/actions/artifact-size-metrics/utils/build-and-upload/metrics.sh
+++ .github/actions/artifact-size-metrics/utils/build-and-upload/metrics.sh
@@ -1,19 +1,30 @@
 # Gets artifact size metrics from staging dir
 getArtifactSizes() {
+  # Staging dir
+  input="build/m2"
+
   # Create output file
   output="build/reports/metrics/artifact-size-metrics.csv"
   mkdir -p "$(dirname "$output")"
   touch "$output"
 
   # Write CSV header
-  echo "Jar File,Size (Bytes)" > "$output"
+  echo "Artifact, Size (Bytes)" > "$output"
 
   # Find all JARs (exclude sources and javadoc)
   # TODO: Calculate KN artifacts sizes
-  find build/m2 -type f -name "*.jar" ! -name "*-sources.jar" ! -name "*-javadoc.jar" | while read -r jar; do
+  find "$input" -type f -name "*.jar" ! -name "*-sources.jar" ! -name "*-javadoc.jar" | while read -r jar; do
       size=$(stat -c%s "$jar")
-      # strip "-<version>(-timestamp?)" before ".jar"
-      artifact=$(echo "$jar" | sed -E 's/-[0-9]+(\.[0-9]+)*(-[0-9]{8}\.[0-9]{6}-[0-9]+)?\.jar$/.jar/')
-      echo "\"$artifact\",$size" >> "$output"
+#      size=$(stat -f%z "$jar")  # TODO: Remove when done testing
+
+      # remove dir path, version, optional timestamp, and .jar
+      artifact=$(basename "$jar")
+      artifact=$(echo "$artifact" | sed -E 's/-[0-9].*\.jar$//')
+
+      # Add JAR to CSV
+      echo "$artifact, $size" >> "$output"
   done
+
+  # Print results for debugging
+  cat "$output"
 }
