a1fcbbf3eef6e73b38b4ab1a3b146d8e6467d841
Matas Lauzadis <lauzmata@amazon.com>
2026-01-15 16:17:02 -0500

    add <type>klib</type> to native dependencies in POMs

--- build-plugins/build-support/src/main/kotlin/aws/sdk/kotlin/gradle/dsl/Publish.kt
+++ build-plugins/build-support/src/main/kotlin/aws/sdk/kotlin/gradle/dsl/Publish.kt
@@ -98,6 +98,7 @@ fun Project.configurePublishing(repoName: String, githubOrganization: String = "
 
         publications.all {
             if (this !is MavenPublication) return@all
+            val publication = this
 
             project.afterEvaluate {
                 pom {
@@ -123,6 +124,34 @@ fun Project.configurePublishing(repoName: String, githubOrganization: String = "
                     }
 
                     artifact(javadocJar)
+
+                    // Add <type>klib</type> for Native platform dependencies
+                    if (publication.name in ALLOWED_KOTLIN_NATIVE_PUBLICATION_NAMES) {
+                        withXml {
+                            val depsNode = asNode().get("dependencies")
+                            if (depsNode == null || (depsNode as groovy.util.NodeList).isEmpty()) {
+                                project.logger.info("No dependencies node found for native publication ${publication.name}")
+                                return@withXml
+                            }
+                            
+                            val deps = (depsNode.first() as groovy.util.Node).children()
+                            
+                            deps.forEach { dep ->
+                                val node = dep as groovy.util.Node
+                                val artifactId = (node.get("artifactId") as? groovy.util.NodeList)
+                                    ?.firstOrNull()
+                                    ?.let { (it as groovy.util.Node).text() }
+                                
+                                if (artifactId != null && ALLOWED_KOTLIN_NATIVE_PUBLICATION_NAMES.any { artifactId.endsWith("-$it", ignoreCase = true) }) {
+                                    val existingType = node.get("type") as? groovy.util.NodeList
+                                    if (existingType == null || existingType.isEmpty()) {
+                                        project.logger.info("Adding <type>klib</type> to dependency on $artifactId")
+                                        node.appendNode("type", "klib")
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
