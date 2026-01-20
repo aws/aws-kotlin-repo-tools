3629595ff9858a65f0bd64b6c1a05ee80f995e1a
0marperez <operez899999@gmail.com>
2026-01-20 16:50:50 -0500

    fix: remove slashes from artifact size metrics file names

--- .github/actions/artifact-size-metrics/utils/calculate-and-upload/main.sh
+++ .github/actions/artifact-size-metrics/utils/calculate-and-upload/main.sh
@@ -13,17 +13,20 @@ setup
 # Calculate size for artifacts in staging dir (build/m2) and save them to metrics_file
 calculateArtifactSizes "$metrics_file" # see: constants.sh
 
+# Remove the org name from the GitHub repository. For example: aws/aws-sdk-kotlin -> aws-sdk-kotlin
+repo_no_org=${GITHUB_REPOSITORY#*/}
+
 # Upload metrics to S3/cloudwatch if required
 if [ "$UPLOAD" == "true" ]; then
   if [ "$RELEASE_METRICS" == "true" ]; then
     # For record-keeping
-    uploadToMetricsBucket "$metrics_file" "$GITHUB_REPOSITORY"-"$IDENTIFIER".csv
-    uploadToMetricsBucket "$metrics_file" "$GITHUB_REPOSITORY"-latest.csv
+    uploadToMetricsBucket "$metrics_file" "$repo_no_org"-"$IDENTIFIER".csv
+    uploadToMetricsBucket "$metrics_file" "$repo_no_org"-latest.csv
 
     # For display in our OPS dashboard
-    uploadToCloudwatch "$metrics_file" "$GITHUB_REPOSITORY"
+    uploadToCloudwatch "$metrics_file" "$repo_no_org"
   else
     # For downstream consumption in pull requests
-    uploadToMetricsBucket "$metrics_file" [TEMP]"$GITHUB_REPOSITORY"-pull-request-"$IDENTIFIER".csv
+    uploadToMetricsBucket "$metrics_file" [TEMP]"$repo_no_org"-pull-request-"$IDENTIFIER".csv
   fi
 fi
