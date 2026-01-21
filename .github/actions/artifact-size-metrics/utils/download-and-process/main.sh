104beb53f4997af191ac7acecadef71fba3ebfb3
0marperez <60363173+0marperez@users.noreply.github.com>
2026-01-21 12:38:49 -0500

    fix: remove slashes from artifact size metrics file names (#163)

--- .github/actions/artifact-size-metrics/utils/download-and-process/main.sh
+++ .github/actions/artifact-size-metrics/utils/download-and-process/main.sh
@@ -9,13 +9,16 @@ source "$(dirname "$0")/../setup.sh"
 
 setup
 
+# Remove the org name from the GitHub repository. For example: aws/aws-sdk-kotlin -> aws-sdk-kotlin
+repo_no_org=${GITHUB_REPOSITORY#*/}
+
 if [ "$DOWNLOAD" == "true" ]; then
   # Get metrics calculated in codebuild - otherwise metrics will already be here
-  downloadFromMetricsBucket [TEMP]"$GITHUB_REPOSITORY"-pull-request-"$IDENTIFIER".csv "$metrics_file" # see: constants.sh
+  downloadFromMetricsBucket [TEMP]"$repo_no_org"-pull-request-"$IDENTIFIER".csv "$metrics_file" # see: constants.sh
 fi
 
 # Metrics from the latest release are never calculated here so we need to download them
-downloadFromMetricsBucket "$GITHUB_REPOSITORY"-latest.csv "$latest_release_metrics_file" # see: constants.sh
+downloadFromMetricsBucket "$repo_no_org"-latest.csv "$latest_release_metrics_file" # see: constants.sh
 
 # Compare metrics
 export LARGE_DIFF=$(compareMetrics "$metrics_file" "$latest_release_metrics_file" "$metrics_comparison_file") # see: constants.sh
