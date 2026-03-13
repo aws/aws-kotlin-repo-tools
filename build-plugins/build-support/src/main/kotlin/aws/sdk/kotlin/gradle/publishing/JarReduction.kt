27400b173dc300e9e7f66daff2e4aacac87ea427
0marperez <60363173+0marperez@users.noreply.github.com>
2026-03-13 10:39:51 -0400

    feat: always enable JAR reduction (#171)

--- build-plugins/build-support/src/main/kotlin/aws/sdk/kotlin/gradle/publishing/JarReduction.kt
+++ build-plugins/build-support/src/main/kotlin/aws/sdk/kotlin/gradle/publishing/JarReduction.kt
@@ -6,7 +6,6 @@
 package aws.sdk.kotlin.gradle.publishing
 
 import aws.sdk.kotlin.gradle.util.getOrNull
-import aws.sdk.kotlin.gradle.util.prop
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.publish.PublishingExtension
@@ -20,9 +19,6 @@ import proguard.gradle.ProGuardTask
 import java.io.File
 
 fun Project.configureJarReduction(group: String) {
-    val jarReductionEnabled = prop("aws.sdk.proguard") as String? == "true"
-    if (!jarReductionEnabled) return
-
     val testLocalJarReplacementTasks = mutableSetOf<Task>()
     val mavenLocalJarReplacementTasks = mutableSetOf<Task>()
 
